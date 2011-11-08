3f055e228958e8f03f5af4b422ebda5fda2e56e0
Blaise Ritchie <rippinblaise@gmail.com>
2011-11-08 14:13:20 -0800

    (hopefully) got search fully working

--- search.php
+++ search.php
@@ -44,14 +44,18 @@
 </div>
 <div id="content">
 <?php
-$query = "SELECT filename, tags FROM minibooru ";
 if(isset($_GET['q'])) {
   $keywords = explode(" ", $_GET['q']);
-  $query .= "WHERE tags LIKE \"%$keywords[0]%\" ";
+  $query = "WHERE tags LIKE \"%$keywords[0]%\" ";
   for($i = 1; $i < count($keywords); $i++) {
     $query .= "AND tags LIKE \"%$keywords[$i]%\" ";
   }
 }
+else {
+  $query = "";
+}
+$numimagesquery = "SELECT COUNT(*) FROM minibooru " . $query;
+$query = "SELECT filename, tags FROM minibooru " . $query;
 if(isset($_GET['s'])) {
   switch($_GET['s']) {
     case "new":
@@ -68,15 +72,70 @@ if(isset($_GET['s'])) {
 else {
   $query .= "ORDER BY date DESC ";
 }
-$query .= "LIMIT 10";
-$result = mysql_query($query) or die(mysql_error());
-if(!($row = mysql_fetch_array($result)))
+if(isset($_GET['p'])) {
+  $page = $_GET['p'] * 20;
+}
+else {
+  $page = 0;
+}
+$query .= "LIMIT ";
+$query .= "$page";
+$query .= ",20";
+$numimagesres = mysql_query($numimagesquery) or die(mysql_error());
+$numimagesarr = mysql_fetch_array($numimagesres);
+$numimages = $numimagesarr[0];
+if($numimages == 0)
   echo "<h2>Sorry! Nothing tagged with your search terms!</h2>";
+else
+  echo "Number of results: $numimages\n<br />\n<div id=\"thumbs\">\n";
+$result = mysql_query($query) or die(mysql_error());
+$row = mysql_fetch_array($result);
 while($row) {
-  echo "<span class=\"thumb\"><a href=\"images/$row[filename]\"><img src=\"thumbnails/$row[filename]\" alt=\"$row[tags]\" title=\"$row[tags]\"></a></span>";
+  echo "<span class=\"thumb\"><a href=\"images/$row[filename]\"><img src=\"thumbnails/$row[filename]\" alt=\"$row[tags]\" title=\"$row[tags]\"></a></span>\n";
   $row = mysql_fetch_array($result);
 }
+echo "</div>\n<div id=\"pages\">\n";
+$numpages = floor($numimages / 20);
+$page /= 20;
+if($page > 0) {
+  echo "<a href=\"search.php?p=" . $page - 1;
+  if(isset($_GET['q']))
+    echo "&q=" . $_GET['q'];
+  if(isset($_GET['s']))
+    echo "&s=" . $_GET['s'];
+  echo "\"><</a>\n";
+}
+else {
+  echo "<\n";
+}
+if($numpages != 0) {
+  for($i = $numpages - 5, $numprinted = 0; $i <= $numpages, $numprinted <= 10; $i++, $numprinted++) {
+    if($i < 0)
+      $i = 0;
+    echo "<a href=\"search.php?p=$i";
+    if(isset($_GET['q']))
+      echo "&q=" . $_GET['q'];
+    if(isset($_GET['s']))
+      echo "&s=" . $_GET['s'];
+    echo "\">$i</a>\n";
+  }
+}
+else {
+  echo "0\n";
+}
+if($numpages > $page) {
+  echo "<a href=\"search.php?p=" . $page + 1;
+  if(isset($_GET['q']))
+    echo "&q=" . $_GET['q'];
+  if(isset($_GET['s']))
+    echo "&s=" . $_GET['s'];
+  echo "\">></a>\n";
+}
+else {
+  echo ">\n";
+}
 ?>
+</div>
 </div>
 </body>
 </html>
