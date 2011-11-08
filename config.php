ffc7da75e678ee7c523b7e7d851f6ff84eb8f528
Blaise Ritchie <rippinblaise@gmail.com>
2011-11-07 21:46:11 -0800

    Removed default config file. Now you must go through the wizard

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $title = "My Booru";
-  $imagedir = "/images/";
-
-  $mysql_host = "localhost";
-  $mysql_database = "mybooru";
-  $mysql_user = "mybooru";
-  $mysql_password = "mybooru";
-?>
